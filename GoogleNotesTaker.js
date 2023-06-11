842f1582fccc7aaec3fd80c48a749090db038d03
Martin York <myork@indeed.com>
2023-06-10 23:00:25 -0700

    GreasyFork version

--- GoogleNotesTaker.js
+++ GoogleNotesTaker.js
@@ -4,6 +4,7 @@
 // @version      0.15
 // @description  Link a private google doc to any web page. Link multiple pages to a single note.
 // @author       Loki Astari
+// @license      MIT
 // @match        https://docs.google.com/document/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_addStyle
@@ -11,10 +12,10 @@
 // @noframes
 // @sandbox      JavaScript
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js#sha384=Q96qXpLdPU1SBEdvTZkaSZfHRsUwS+sj/WFUdmYvGhBNtwYUucjuwhZT6glwdVXk%
+// @require      https://raw.githubusercontent.com/Loki-Astari/TampermonkeyScripts/master/GoogleNotesTaker/UI.js#sha384=TRKVtP91j4B5qAQFBUwb487cX87yTUMBuPkjYt7S+AKCDX4O0s/b/lppq9C59RUq%
 // ==/UserScript==
 
 (function() {
-    GoogleDocsNoteTaker();
+     GoogleDocsNoteTaker();
 })();
-
