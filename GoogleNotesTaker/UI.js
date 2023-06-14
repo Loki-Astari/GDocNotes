258b8ebdc0b5df22067e4f873cc7cb592533e51d
Loki Astari <Loki.Astari@gmail.com>
2023-06-14 08:37:18 -0700

    Update UI.js
    Recursive inclusion of script removed.

--- GoogleNotesTaker/UI.js
+++ GoogleNotesTaker/UI.js
@@ -1,18 +1,3 @@
-// ==UserScript==
-// @name         Google Docs Note Taker
-// @namespace    http://LokiAstari.com/
-// @version      0.15
-// @description  Link a private google doc to any web page. Link multiple pages to a single note.
-// @author       Loki Astari
-// @match        https://docs.google.com/document/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        GM_addStyle
-// @grant        unsafeWindow
-// @noframes
-// @sandbox      JavaScript
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
-// ==/UserScript==
 
 /*
  * Definitions:
